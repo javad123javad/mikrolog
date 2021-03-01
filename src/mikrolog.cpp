7cc191e7cd9f2748033898af23eca3debd58af97
Javad Rahimi <javad321javad@gmail.com>
2021-03-01 10:21:13 +0330

    test logs added

--- src/mikrolog.cpp
+++ src/mikrolog.cpp
@@ -16,6 +16,10 @@ using namespace std;
 int main(void) {
 	logger_nm::logger *logging = logger_nm::logger::get_instance("dymmy.log",logger_nm::LOG_TRACE);//get_instance("dummy_log.log", logger_nm::LOG_TRACE);
 	logging->log_info("%s:%d %s", __FILE__, __LINE__, "brake or steer not in center_position");
+	logging->log_trace("%s:%d %s", __FILE__, __LINE__, "brake or steer not in center_position");
+	logging->log_warn("%s:%d %s", __FILE__, __LINE__, "brake or steer not in center_position");
+	logging->log_error("%s:%d %s", __FILE__, __LINE__, "brake or steer not in center_position");
+
 	cout << "Mikrolog" << endl;  /* prints Mikrolog */
 	return 0;
 }
